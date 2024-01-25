83b522ab0a8a5f7bd575882e430f10113cbfd9a1
Zobrig <zobrigg@gmail.com>
2024-01-25 14:37:01 +0200

    ya tikal knopki randomno i ono zarabotalo

---
+++ myprj.ts
@@ -0,0 +1,18 @@
+let a = 10
+let b = 10
+console.log(a == b)
+
+a = 10
+b = "10"
+console.log(a === b)
+
+const myObj = {};
+
+console.log(typeof "a")
+console.log(typeof 1)
+console.log(typeof Symbol('гыыыы'))
+console.log(typeof(a == b))
+console.log(typeof c)
+console.log(typeof null)
+console.log(typeof myObj)
+console.log(null)
