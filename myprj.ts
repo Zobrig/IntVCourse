154928efa110f7d1fde9458832afcb8cff0ee6c9
Zobrig <zobrigg@gmail.com>
2024-01-26 14:57:11 +0200

    lol

--- myprj.ts
+++ myprj.ts
@@ -19,60 +19,92 @@
 // console.log(null)
 
 //-----------------HOME WORK 1.2------------------
-// 1 ВАРІАНТ
-let age = 15
+// // 1 ВАРІАНТ
+// let age = 15
 
-function canSmoke(age){
-	if(age >= 18){
-		return("Покупка сигарет успішна")
-	}
-	if(age >= 100){
-		return("Не продам ти старий")
-	}
-		return("Не продам ти замалий")
+// function canSmoke(age){
+// 	if(age >= 18){
+// 		return("Покупка сигарет успішна")
+// 	}
+// 	if(age >= 100){
+// 		return("Не продам ти старий")
+// 	}
+// 		return("Не продам ти замалий")
+// }
+// console.log(canSmoke(age))
+
+// // 2 ВАРІАНТ
+
+// let agee = 19
+
+// let canSmokee = function(agee){
+// 	if(agee >= 18){
+// 		return("Покупка сигарет успішна")
+// 	}
+// 	if(agee >= 100){
+// 		return("Не продам ти старий")
+// 	}
+// 		return("Не продам ти замалий")
+// }
+// console.log(canSmokee(agee))
+
+// // 3 ВАРІАНТ 
+
+// let ageee = 119
+
+// let canSmokeee = (ageee) => {
+// 	if(ageee >= 18){
+// 		return("Покупка сигарет успішна")
+// 	}
+// 	if(ageee >= 100){
+// 		return("Не продам ти старий")
+// 	}
+// 		return("Не продам ти замалий")
+// }
+// console.log(canSmokeee(ageee))
+
+// //--------- allowedTOIn ПЕРЕПУСТКА -----------
+
+// const ageAllowe = 72
+// const name = "John" 
+// const department = "Police"
+
+// let allowedTOIn = (ageAllowe,name,department) => {
+// 	if(ageAllowe === 72 && name === "John" && department === "Police"){
+// 		return("Вхід в систему дозволений")
+// 	}
+// 	return("Вхід в систему закритий")
+// }
+// console.log(allowedTOIn(ageAllowe,name,department))
+
+//-----------------HOME WORK 2.1------------------
+
+// реалізувати реверс стрінги (вивод в зворотньому порядку)
+function reverse(str) {
+  return str.split('').reverse().join('');
 }
-console.log(canSmoke(age))
 
-// 2 ВАРІАНТ
+const reversedString = reverse('igor');
+console.log(reversedString);
 
-let agee = 19
+//реалізувати функцію поліндром (коли в обидва боки одинакова стрінга)
 
-let canSmokee = function(agee){
-	if(agee >= 18){
-		return("Покупка сигарет успішна")
-	}
-	if(agee >= 100){
-		return("Не продам ти старий")
-	}
-		return("Не продам ти замалий")
+function isPalindrome(str) {
+  const cleanedStr = str.replace(/\s/g, '').toLowerCase();
+  
+  return cleanedStr === cleanedStr.split('').reverse().join('');
 }
-console.log(canSmokee(agee))
 
-// 3 ВАРІАНТ 
+console.log(isPalindrome("aba"));
+console.log(isPalindrome("abc"));
 
-let ageee = 119
+//вивести тільки парні числа з масиву, вивести в зворотньому порядку
 
-let canSmokeee = (ageee) => {
-	if(ageee >= 18){
-		return("Покупка сигарет успішна")
-	}
-	if(ageee >= 100){
-		return("Не продам ти старий")
-	}
-		return("Не продам ти замалий")
+function getPaired(arr) {
+	const pairedNumbers = arr.filter(number => number % 2 === 0);
+
+	return pairedNumbers.reverse();
 }
-console.log(canSmokeee(ageee))
 
-//--------- allowedTOIn ПЕРЕПУСТКА -----------
-
-const ageAllowe = 72
-const name = "John" 
-const department = "Police"
-
-let allowedTOIn = (ageAllowe,name,department) => {
-	if(ageAllowe === 72 && name === "John" && department === "Police"){
-		return("Вхід в систему дозволений")
-	}
-	return("Вхід в систему закритий")
-}
-console.log(allowedTOIn(ageAllowe,name,department))
+const result = getPaired([1, 2, 3, 4, 5, 6]);
+console.log(result);
